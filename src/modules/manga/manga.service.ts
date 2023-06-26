854b3bbe6a6e756334498b0f5e0ecc94b7a9386a
ChatonexX <122827963+Chetos26@users.noreply.github.com>
2023-06-26 09:29:18 -0500

    manga service

--- src/modules/manga/manga.service.ts
+++ src/modules/manga/manga.service.ts
@@ -4,23 +4,78 @@ import { UpdateMangaDto } from './dto/update-manga.dto';
 
 @Injectable()
 export class MangaService {
-  create(createMangaDto: CreateMangaDto) {
-    return 'This action adds a new manga';
-  }
-
-  findAll() {
-    return `This action returns all manga`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} manga`;
-  }
-
-  update(id: number, updateMangaDto: UpdateMangaDto) {
-    return `This action updates a #${id} manga`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} manga`;
-  }
+  constructor(
+    @InjectRepository(Manga) private mangaRepostory: Repository<Manga>
+    ){}
+    async create(createmangaDto: CreateMangaDto):Promise<Manga> {
+      try {
+          const manga: Manga = await this.mangaRepostory.save(createmangaDto);
+          return manga;
+        } catch (e) {
+          throw new Error(e);
+      }
+    }
+  
+    async findAll():Promise<Manga[]> {
+      try {
+          const mangas: Manga[] =await this.mangaRepostory.find();
+          if (mangas.length === 0) {
+            throw new error({
+              type:'BAD_REQUEST',
+              message:'No existen registros'
+            });
+          }
+          return mangas
+        } catch (e) {
+         console.log(e);
+         
+      }
+    }
+  
+    async findOne(id: string):Promise<Manga> {
+      try {
+          const manga: Manga = await this.mangaRepostory.createQueryBuilder('maestro').where({id}).getOne()
+          if (!manga) {
+            throw new error({
+              type:'BAD_REQUEST',
+              message:'No se ha encontrado el registro'
+            });
+          }
+          return manga;
+        } catch (e) {
+          console.log(e);
+          
+      }
+    }
+    async update(id: string, updateMangaDto: UpdateMangaDto):Promise<UpdateResult | undefined> {
+     try {
+         const manga: UpdateResult =  await this.mangaRepostory.update(id,updatemangaDto);
+         if (manga.affected === 0) {
+          throw new error({
+            type:'BAD_REQUEST',
+            message:'No se ha podido actualizar'
+          });
+         }
+          return manga;
+       } catch (e) {
+        console.log(e);
+        
+     }
+    }
+  
+    async remove(id: string):Promise<DeleteResult | undefined> {
+      try {
+          const manga: DeleteResult =  await this.mangaRepostory.delete(id);
+          if (manga.affected === 0) {
+            throw new error({
+              type:'BAD_REQUEST',
+              message:'No se ha podido borrar'
+            });
+          }
+            return manga;
+        } catch (e) {
+          console.log(e);
+          
+      }
+     }
 }

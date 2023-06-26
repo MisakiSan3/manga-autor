4720ed595b3d46a67de803154d30767830102ce4
ChatonexX <122827963+Chetos26@users.noreply.github.com>
2023-06-26 09:05:10 -0500

    .

--- src/modules/manga/dto/create-manga.dto.ts
+++ src/modules/manga/dto/create-manga.dto.ts
@@ -1,1 +1,44 @@
-export class CreateMangaDto {}
+import { Autor } from "src/modules/autor/entities/autor.entity";
+
+export class CreateMangaDto {
+
+    @IsOptional()
+    @IsDate()
+    createAt: Date;
+
+    @IsOptional()
+    @IsDate()
+    updateAt: Date;    
+    
+    @IsOptional()
+    @IsDate()
+    deleteAt: Date;
+
+    
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    code: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    age: number;
+    
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    identificacion: string;
+
+    @IsOptional()
+    @IsDate()
+    birthday: Date;
+
+    @IsNotEmptyObject()
+    autor:Autor
+}
--- src/modules/manga/dto/update-manga.dto.ts
+++ src/modules/manga/dto/update-manga.dto.ts
@@ -1,4 +1,45 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMangaDto } from './create-manga.dto';
+import { Autor } from 'src/modules/autor/entities/autor.entity';
 
-export class UpdateMangaDto extends PartialType(CreateMangaDto) {}
+export class UpdateMangaDto extends PartialType(CreateMangaDto) {
+    @IsOptional()
+    @IsDate()
+    createAt: Date;
+
+    @IsOptional()
+    @IsDate()
+    updateAt: Date;    
+    
+    @IsOptional()
+    @IsDate()
+    deleteAt: Date;
+
+    
+    @IsString()
+    @IsOptional()
+    nombre: string;
+
+    @IsNumber()
+    @IsOptional()
+    code: number;
+
+    @IsNumber()
+    @IsOptional()
+    age: number;
+    
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    identificacion: string;
+
+    @IsOptional()
+    @IsDate()
+    birthday: Date;
+
+    @IsOptional()
+    autor:Autor
+}
